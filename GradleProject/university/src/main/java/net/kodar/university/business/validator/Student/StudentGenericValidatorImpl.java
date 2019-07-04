5295cc3aa5332c369f65867c11044a783b4100e6
kamentr <kamen.traikovbg@gmail.com>
2019-07-04 15:14:00 +0300

    Added Custom Validation to the parameters.

--- GradleProject/university/src/main/java/net/kodar/university/business/validator/Student/StudentGenericValidatorImpl.java
+++ GradleProject/university/src/main/java/net/kodar/university/business/validator/Student/StudentGenericValidatorImpl.java
@@ -4,11 +4,18 @@ import net.kodar.university.business.validator.GenericValidatorImpl;
 import net.kodar.university.presentation.depricated.parameter.StudentParam;
 import org.springframework.stereotype.Component;
 
+import javax.validation.ValidationException;
+import javax.validation.Validator;
+
 @Component
 public class StudentGenericValidatorImpl extends GenericValidatorImpl<StudentParam> implements StudentGenericValidator {
 
     @Override
     public void validate(StudentParam obj) {
         super.validate(obj);
+
+        if(obj.getFirstName().equals("name")){
+            throw new ValidationException("Name cannot be left");
+        }
     }
 }
